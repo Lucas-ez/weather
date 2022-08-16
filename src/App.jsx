96888a5a780e38afd316389f0bacb91b635666d3
Lucas-ez <lucasezequiel237@gmail.com>
2022-08-16 17:33:34 -0300

    Edit css del buscador

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,19 @@ import './App.css';
 import { fetchWeather } from './api/fetchWeather';
 import { useState } from 'react';
 
+const DESCRIPCIONES = {
+  "clear sky": "Cielo despejado",
+  "few clouds": "Parcialmente Nublado",
+  "scattered clouds": "Nublado",
+  "broken clouds": "Nublado",
+  "shower rain": "Lluvia",
+  "rain": "Lluvia",
+  "heavy intensity rain": "Tormenta",
+  "thunderstorm": "Tormenta eléctrica",
+  "snow": "Nieve",
+  "mist": "Niebla"
+}
+
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
@@ -14,19 +27,29 @@ function App() {
     }
   }
 
+  console.log(weather);
+
   return (
     <div className="App">
-      <input type="text" placeholder='Search...' value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}/>
-      {weather.main && (
-        <div>
-          <div>
-            {Math.round(weather.main.temp)}
-            <span>&deg;C</span>
+      <div className='container'>
+        <input className='buscador' type="text" placeholder='Search...' value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}/>
+        {weather.main && (
+          <div className='weather-card'>
+            <div className='temperatura'>
+              {Math.round(weather.main.temp)}
+              <span>&deg;C</span>
+            </div>
+            <div className='clima'>
+              <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={`${weather.weather[0].main}`} />
+              <span>{DESCRIPCIONES[weather.weather[0].description]}</span>
+            </div>
+            <div className='ubicacion'>
+              <span>{weather.name}</span>
+              <sup>{weather.sys.country}</sup>
+            </div>
           </div>
-          <div>{weather.name}</div>
-          <div>{weather.sys.country}</div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
